ecd667a34ea054b58d2ccc33f23e53e4a6289ba8
John Michael Songco <jmgs21x@gmail.com>
2026-01-29 21:14:27 +0800

    First Commit

---
+++ main/index.js
@@ -0,0 +1,6 @@
+const loginpage = document.querySelector(".main-box");
+const loginbutton = document.querySelector("#login-button");
+
+loginbutton.addEventListener("click", (event) => {
+    loginpage.style.display = "none";
+});
